c4c4e05cb704341354df4f9333cbd2c8a9cb64a6
Sara <saragorleta@yahoo.com>
2021-02-03 14:41:36 +0100

    prova "scrivi messaggio" e aggiungilo alla chat

--- js/script.js
+++ js/script.js
@@ -1,32 +1,96 @@
 var app = new Vue({
     el:"#app",
     data:{
-      
+      arrayVuoto:'',
       indexUtenteSelezionato:0,
 
-      mioProfilo:
-
-        {
+      mioProfilo:{
           nome:'Giorgia',
-         avatar:'img/avatar_6.jpg'
-        }
-      ,
+         avatar:'img/avatar_6.jpg',
+          },
+
       contatti:[
           {
             nome:'Michele',
-            avatar:'img/avatar_1.jpg'
+            avatar:'img/avatar_1.jpg',
+            visible:'true',
+            messages: [
+             {
+               date: '10/01/2020 15:30:55',
+               text: 'Hai portato a spasso il cane?',
+               status: 'sent'
+             },
+             {
+               date: '10/01/2020 15:50:00',
+               text: 'Ricordati di dargli da mangiare',
+               status: 'sent'
+             },
+             {
+               date: '10/01/2020 16:15:22',
+               text: 'Tutto fatto!',
+               status: 'received'
+             }
+               ],
           },
+
           {
             nome:'Giovanni',
-            avatar:'img/avatar_2.jpg'
+            avatar:'img/avatar_2.jpg',
+            messages: [
+             {
+               date: '10/01/2020 15:30:55',
+               text: 'Ciao come stai?',
+               status: 'sent'
+             },
+             {
+               date: '10/01/2020 15:50:00',
+               text: 'Bene grazie stasera ci vediamo?',
+               status: 'received'
+             },
+             {
+               date: '10/01/2020 16:15:22',
+               text: 'Mi piacerebbe ma devo andare a fare la spesa',
+               status: 'sent'
+             }
+               ],
           },
           {
             nome:'Tommaso',
-            avatar:'img/avatar_3.jpg'
+            avatar:'img/avatar_3.jpg',
+            messages: [
+             {
+               date: '10/01/2020 15:30:55',
+               text: 'La Marianna va in campagna',
+               status: 'received'
+             },
+             {
+               date: '10/01/2020 15:50:00',
+               text: 'Sicuro di non aver sbagliato chat?',
+               status: 'sent'
+             },
+             {
+               date: '10/01/2020 16:15:22',
+               text: 'Hai ragione scusami',
+               status: 'received'
+             }
+               ],
           },
           {
             nome:'Giulio',
-            avatar:'img/avatar_4.jpg'
+            avatar:'img/avatar_4.jpg',
+            messages: [
+             {
+               date: '10/01/2020 15:30:55',
+               text: 'Lo sai che ha aperto una nuova pizzeria',
+               status: 'sent'
+             },
+             {
+               date: '10/01/2020 15:50:00',
+               text: 'si ma preferisco andare al cinema',
+               status: 'received'
+             },
+
+               ],
           },
       ]
     },
@@ -34,6 +98,10 @@ var app = new Vue({
       aggiungiContatto(index){
         this.indexUtenteSelezionato=index;
         console.log(index);
+      },
+      aggiungiTesto(){
+        this.contatti.push(this.arrayVuoto);
+        this.arrayVuoto='';
       }
     }
   });
