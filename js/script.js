4e1b02853d314071cf01b6a9b98ba2ebc2b6f7f9
Sara <saragorleta@yahoo.com>
2021-02-04 13:28:49 +0100

    risposta ok con funzione

--- js/script.js
+++ js/script.js
@@ -15,17 +15,17 @@ var app = new Vue({
 
             messages: [
              {
-               date: '10/01/2020 15:30:55',
+               date: moment().format(' h:mm:ss'),
                text: 'Hai portato a spasso il cane?',
                status: 'sent'
              },
              {
-               date: '10/01/2020 15:50:00',
+               date: moment().format(' h:mm:ss'),
                text: 'Ricordati di dargli da mangiare',
                status: 'sent'
              },
              {
-               date: '10/01/2020 16:15:22',
+               date: moment().format(' h:mm:ss'),
                text: 'Tutto fatto!',
                status: 'received'
              }
@@ -37,17 +37,17 @@ var app = new Vue({
             avatar:'img/avatar_2.jpg',
             messages: [
              {
-               date: '10/01/2020 15:30:55',
+               date: moment().format(' h:mm:ss'),
                text: 'Ciao come stai?',
                status: 'sent'
              },
              {
-               date: '10/01/2020 15:50:00',
+               date: moment().format(' h:mm:ss'),
                text: 'Bene grazie stasera ci vediamo?',
                status: 'received'
              },
              {
-               date: '10/01/2020 16:15:22',
+               date: moment().format(' h:mm:ss'),
                text: 'Mi piacerebbe ma devo andare a fare la spesa',
                status: 'sent'
              }
@@ -58,17 +58,17 @@ var app = new Vue({
             avatar:'img/avatar_3.jpg',
             messages: [
              {
-               date: '10/01/2020 15:30:55',
+               date: moment().format(' h:mm:ss'),
                text: 'La Marianna va in campagna',
                status: 'received'
              },
              {
-               date: '10/01/2020 15:50:00',
+               date: moment().format(' h:mm:ss'),
                text: 'Sicuro di non aver sbagliato chat?',
                status: 'sent'
              },
              {
-               date: '10/01/2020 16:15:22',
+               date: moment().format(' h:mm:ss'),
                text: 'Hai ragione scusami',
                status: 'received'
              }
@@ -79,12 +79,12 @@ var app = new Vue({
             avatar:'img/avatar_4.jpg',
             messages: [
              {
-               date: '10/01/2020 15:30:55',
+               date: moment().format(' h:mm:ss'),
                text: 'Lo sai che ha aperto una nuova pizzeria',
                status: 'sent'
              },
              {
-               date: '10/01/2020 15:50:00',
+               date: moment().format(' h:mm:ss'),
                text: 'si ma preferisco andare al cinema',
                status: 'received'
              },
@@ -112,7 +112,7 @@ var app = new Vue({
           status: 'sent'
           }
         this.contatti[this.indexUtenteSelezionato].messages.push(nuovoMessaggio);
-        setTimeOut(aggiungiOk,2000);
+        setTimeout(this.aggiungiOk,2000);
       },
 
         aggiungiOk(){
@@ -122,9 +122,23 @@ var app = new Vue({
           status: 'received'
         }
         this.contatti[this.indexUtenteSelezionato].messages.push(rispostaMessaggio);
+
       },
-      // contatti.forEach((element, index) => {
-      //  this.nome
+      // var ages = [32, 33, 16, 40];
+      //
+      // function checkAdult(age) {
+      //   return age >= 18;
+      // }
+      //
+      // function myFunction() {
+      //   document.getElementById("demo").innerHTML = ages.filter(checkAdult);
+      // }
+
+      //***************
+      // function contattoFiltrato(){
+      //   const contact=this.contatti;
+      //   return contact.filter();
+      //}
       // });
     }
   });
